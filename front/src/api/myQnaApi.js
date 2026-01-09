e13da6ccb5433fb608449cb405e7b2fe59fb982d
ProjectBA0 <bingsano@naver.com>
2026-01-09 16:51:08 +0900

    5

--- front/src/api/myQnaApi.js
+++
@@ -1,7 +0,0 @@
-import client from "./client";
-
-// 나의 문의내역 가져오기
-export async function getMyQna() {
-  const res = await client.get("/api/me/qna");
-  return res.data;
-}
--- front/src/api/myReviewApi.js
+++
@@ -1,7 +0,0 @@
-// src/api/myReviewApi.js
-import client from "./client";
-
-export async function getMyReviews() {
-  const res = await client.get("/api/me/reviews");
-  return res.data;
-}
